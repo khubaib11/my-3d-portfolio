f4bdd8ffdb1f23fff2595e91a333ca2df7d484dc
khubaib khan <khankhubaib089@gmail.com>
2025-08-20 01:26:06 +0500

    Fix GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/AI-Horizon-Blog-Web/',   // 👈 add this line
   plugins: [tailwindcss(), react()],
   server: {
-    host: '0.0.0.0', // allow external devices
-    port: 5173,      // you can change this if 5173 is blocked
+    host: '0.0.0.0',
+    port: 5173,
   },
 })
